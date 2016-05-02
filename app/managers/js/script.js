b4252e85a8f0dd54b734e78b593bf733edf08170
Aviad Hahami <aviadhahami@gmail.com>
2016-05-02 16:09:08 +0300

    nice

--- app/managers/js/script.js
+++ app/managers/js/script.js
@@ -5,23 +5,17 @@
 $(document).ready(function () {
     var ref = new Firebase("https://skillsetraffle.firebaseio.com/").child('raffleData');
 
-    // ref.push(null);
-    $('#form').submit(function(e){
-        e.preventDefault();
-        var name = $('#name').val();
-        var mail = $('#mail').val();
-        // console.log(name,mail)
-        ref.push({name:name,mail:mail},function(data){
-            $('.content').css('display','none');
-            var res;
-            if(data == null){   
-                // Success
-                res = $('.success');
-            }else{
-                // Error
-                res = $('error');
-            }
-            res.css('display','block');
-        });
-    })
+    var data = [];
+    var populateScreen = function (size) {
+        console.log(size)
+        $('#numOfPeople').html('choosing between <b>' + size + '</b> participants');
+    };
+    ref.once('value', function(dataSnapshot) {
+        // handle read data.
+        var snapshot = dataSnapshot.val();
+        for (var snap in snapshot){
+            data.push(snapshot[snap]);
+        }
+        populateScreen(data.length);
+    });
 });
